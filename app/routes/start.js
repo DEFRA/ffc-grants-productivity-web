b598e694388623e236ca8d7ec5e61efee7b0d048
ellie <ellie.tms1371@gmail.com>
2021-07-19 09:44:55 +0100

    start page updates

--- app/routes/start.js
+++ app/routes/start.js
@@ -2,6 +2,6 @@ module.exports = {
   method: 'GET',
   path: '/productivity/start',
   handler: (request, h) => {
-    return h.view('home', { button: { nextLink: 'project-subject', text: 'Start' } })
+    return h.view('home', { button: { nextLink: 'project-subject', text: 'Start now' } })
   }
 }
