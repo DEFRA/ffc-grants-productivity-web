148e90f0bbc46e6e1b95a07d7c8d330306bf92fe
ogbk <ogbu.k.olu@gmail.com>
2021-07-20 17:45:48 +0100

    redirect / to /productivity/start

--- app/routes/start.js
+++ app/routes/start.js
@@ -1,7 +1,16 @@
-module.exports = {
-  method: 'GET',
-  path: '/productivity/start',
-  handler: (request, h) => {
-    return h.view('home', { button: { nextLink: 'project-subject', text: 'Start now' } })
+module.exports = [
+  {
+    method: 'GET',
+    path: '/productivity/start',
+    handler: (request, h) => {
+      return h.view('home', { button: { nextLink: 'project-subject', text: 'Start now' } })
+    }
+  },
+  {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return h.redirect('/productivity/start')
+    }
   }
-}
+]
