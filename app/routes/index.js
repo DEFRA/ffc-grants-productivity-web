ee0c251f60d75c3a8bfc95b2a513e1e14f43ee2d
ogbk <ogbu.k.olu@gmail.com>
2021-07-28 13:36:22 +0100

    Cleanup code (#39)
    * refactor code
    
    * rename function
    
    * extract input functions away from index
    
    * bump version
    
    * rename function
    
    * rename function

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,91 +1,9 @@
 const questionBank = require('../config/question-bank')
 const { setYarValue, getYarValue } = require('../helpers/session')
 const { getGrantValues } = require('../helpers/grants-info')
+const { radioButtons, checkBoxes, inputText } = require('../helpers/answer-types')
 
-function isChecked (data, option) {
-  return !!data && data.includes(option)
-}
-
-function setAnswerOptions (data, answers) {
-  return answers.map((answer) => {
-    const { value, hint, text } = answer
-
-    if (value === 'divider') {
-      return { divider: 'or' }
-    }
-
-    if (typeof (value) === 'string') {
-      return {
-        value,
-        text: value,
-        hint,
-        checked: isChecked(data, value),
-        selected: data === value
-      }
-    }
-
-    return {
-      value,
-      text,
-      hint,
-      checked: isChecked(data, value),
-      selected: data === value
-    }
-  })
-}
-
-const radioButtons = (data, question) => {
-  const { classes, yarKey, title, answers } = question
-  return {
-    classes,
-    idPrefix: yarKey,
-    name: yarKey,
-    fieldset: {
-      legend: {
-        text: title,
-        isPageHeading: true,
-        classes: 'govuk-fieldset__legend--l'
-      }
-    },
-    items: setAnswerOptions(data, answers)
-  }
-}
-
-const checkBoxes = (data, question) => {
-  const { classes, yarKey, title, hint, answers } = question
-  return {
-    classes,
-    idPrefix: yarKey,
-    name: yarKey,
-    fieldset: {
-      legend: {
-        text: title,
-        isPageHeading: true,
-        classes: 'govuk-fieldset__legend--l'
-      }
-    },
-    hint: {
-      text: hint
-    },
-    items: setAnswerOptions(data, answers)
-  }
-}
-
-const inputText = (data, question) => {
-  const { yarKey, prefix, suffix, label, hint } = question
-  return {
-    id: yarKey,
-    name: yarKey,
-    classes: 'govuk-input--width-10',
-    prefix,
-    suffix,
-    label,
-    hint,
-    value: data || ''
-  }
-}
-
-const getOptions = (data, question) => {
+const getCorrectAnswerType = (data, question) => {
   switch (question.type) {
     case 'single-answer':
       return radioButtons(data, question)
@@ -104,7 +22,7 @@ const getModel = (data, question) => {
   const model = {
     type,
     backUrl,
-    items: getOptions(data, question),
+    items: getCorrectAnswerType(data, question),
     sideBarText: question.sidebar
   }
   return model
@@ -147,11 +65,14 @@ const showPostPage = (currentQuestion, request, h) => {
 
   // based on answer -> redirect to pages [not eligible] or [maybe eligible]
 
-  if (answers.find(answer => (answer.value === value && !answer.isEligible))) {
-    return h.view('not-eligible', NOT_ELIGIBLE)
-  } else if (answers.find(answer => (answer.value === value && answer.isEligible === 'maybe'))) {
-    const thisAnswer = answers.find(answer => (answer.value === value && answer.isEligible === 'maybe'))
-    return h.redirect(thisAnswer.maybeUrl)
+  const thisAnswer = answers.find(answer => (answer.value === value))
+
+  if (thisAnswer) {
+    if (thisAnswer.notEligible) {
+      return h.view('not-eligible', NOT_ELIGIBLE)
+    } else if (thisAnswer.redirectUrl) {
+      return h.redirect(thisAnswer.redirectUrl)
+    }
   }
 
   const errorList = []
