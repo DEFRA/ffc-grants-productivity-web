6bc6582355c3939cb42c16f77f8df57398850144
diya2214 <madihakhn@yahoo.com>
2021-08-18 11:01:43 +0100

    fix test

--- test/integration/narrow/routes/confirm.test.js
+++ test/integration/narrow/routes/confirm.test.js
@@ -25,6 +25,6 @@ describe('confirm page', () => {
 
     const postResponse = await global.__SERVER__.inject(postOptions)
     expect(postResponse.statusCode).toBe(302)
-    expect(postResponse.headers.location).toBe(`${global.__URLPREFIX__}/confirmation`)
+    expect(postResponse.headers.location).toBe('reference-number')
   })
 })
