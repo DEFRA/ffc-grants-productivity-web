3d03b7ba9d3c31101e7bf0c0c7fe17cc05c2c144
Madiha <45498508+diya2214@users.noreply.github.com>
2021-08-24 12:14:02 +0100

    FFTF-1319: Session timeout feature (#80)
    * adding session timeout route
    * session timeout feature

--- app/plugins/cookies.js
+++ app/plugins/cookies.js
@@ -9,18 +9,22 @@ module.exports = {
       server.state('cookies_policy', config)
 
       server.ext('onPreResponse', (request, h) => {
+        let showTimeout = false
+        if (!sessionIgnorePaths.find(path => request.path.startsWith(path)) && request.path !== '/') {
+          showTimeout = true
+          if (!validSession(request) && server.table().filter(route => request.path.toLowerCase() === route.path.toLowerCase()).length > 0) {
+            return h.redirect('session-timeout')
+          }
+        }
+
         const statusCode = request.response.statusCode
         if (request.response.variety === 'view' && statusCode !== 404 && statusCode !== 500 && request.response.source.manager._context) {
           const cookiesPolicy = getCurrentPolicy(request, h)
           request.response.source.manager._context.cookiesPolicy = cookiesPolicy
+          request.response.source.manager._context.showTimeout = showTimeout
           request.response.source.manager._context.sessionTimeoutInMin = ((cacheConfig.expiresIn * 60) / (3600 * 1000)) - 10
         }
 
-        if (!sessionIgnorePaths.find(path => request.path.startsWith(path)) && request.path !== '/') {
-          if (!validSession(request)) {
-            return h.redirect('session-timeout')
-          }
-        }
         return h.continue
       })
     }
