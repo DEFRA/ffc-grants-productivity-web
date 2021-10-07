d091488bd2a2551b0eb9bbe61c9fe52ccf3409a3
Madiha <45498508+diya2214@users.noreply.github.com>
2021-10-07 13:20:58 +0100

    FFTF-1824: code smell fix (#124)
    * code smell fix

--- app/helpers/regex.js
+++ app/helpers/regex.js
@@ -8,7 +8,7 @@ const SELECT_VARIABLE_TO_REPLACE = /{{_(.+?)_}}/ig
 const DELETE_POSTCODE_CHARS_REGEX = /[)(.\s-]*/g
 const POSTCODE_REGEX = /^[\s]*[a-z]{1,2}\d[a-z\d]?[\s]*\d[a-z]{2}[\s]*$/i
 const NAME_ONLY_REGEX = /^[a-zA-Z,' -]*$/
-const PHONE_REGEX = /^[0-9\{\[\(\)\}\]— -+]{10,}$/
+const PHONE_REGEX = /^\+?[0-9\[\s\(\)\]\-]{10,}$/
 const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,5})+$/
 
 module.exports = {
