3a8494b00729701882b4b854794f970c3a9de877
Satish Chatap <satish.chatap@capgemini.com>
2021-10-21 21:03:25 +0100

    Fix test

--- app/helpers/page-guard.js
+++ app/helpers/page-guard.js
@@ -4,9 +4,9 @@ function guardPage (request, guardData, rule = null) {
   let result = false
   if (guardData) {
     if (rule) {
-      result = rule.condition === 'ANY' && [guardData].some(dependcyKey => getYarValue(request, dependcyKey))
+      result = rule.condition === 'ANY' && !guardData.some(dependcyKey => getYarValue(request, dependcyKey) !== null)
     } else {
-      result = [guardData].filter(dependcyKey => getYarValue(request, dependcyKey) === null).length > 0
+      result = guardData.filter(dependcyKey => getYarValue(request, dependcyKey) === null).length > 0
     }
   }
   return result
