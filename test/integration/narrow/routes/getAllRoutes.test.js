de070065a400dc46d131d01829493a633ecac3b5
zavies97 <zavies97@users.noreply.github.com>
2023-05-04 15:06:05 +0100

    more route tests added

---
+++ test/integration/narrow/routes/getAllRoutes.test.js
@@ -0,0 +1,34 @@
+const { ALL_QUESTIONS } = require('../../../../app/config/question-bank')
+let varList
+ALL_QUESTIONS.forEach(question => {
+  if (question.preValidationKeys) {
+    varList = question.preValidationKeys.map(m => {
+      return { m: 'someValue' }
+    })
+  }
+})
+jest.doMock('../../../../app/helpers/session', () => ({
+  setYarValue: (request, key, value) => null,
+  getYarValue: (request, key) => {
+    if (varList[key]) return varList[key]
+    else return 'Error'
+  }
+}))
+
+describe('All default GET routes', () => {
+  varList.planningPermission = 'Not yet applied'
+  varList.PlanningPermissionEvidence = {
+    planningAuthority: 'some planning',
+    planningReferenceNumber: '123456-ref'
+  }
+  ALL_QUESTIONS.forEach(question => {
+    it(`should load ${question.key} page successfully`, async () => {
+      const options = {
+        method: 'GET',
+        url: `${global.__URLPREFIX__}/${question.url}`
+      }
+      const response = await global.__SERVER__.inject(options)
+      expect(response.statusCode).toBe(200)
+    })
+  })
+})
