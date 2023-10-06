44848f80da6d13b1f5a94e5a258a3565d9e40b4c
Yunus FIRAT <yunusfirat44@gmail.com>
2023-10-06 11:29:08 +0100

    put back test.

---
+++ test/integration/narrow/routes/country.test.js
@@ -0,0 +1,47 @@
+const { crumbToken } = require('./test-helper')
+
+describe('country', () => {
+  it('should returns error message if no option is selected', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/country`,
+      payload: { projectPostcode: '', crumb: crumbToken },
+      headers: {
+        cookie: 'crumb=' + crumbToken
+      }
+    }
+
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.statusCode).toBe(200)
+    expect(postResponse.payload).toContain('Select yes if the project is in England')
+  })
+
+  it('should store user response and redirects to planning permission page', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/country`,
+      payload: { inEngland: 'Yes', projectPostcode: 'XX1 5XX', crumb: crumbToken },
+      headers: {
+        cookie: 'crumb=' + crumbToken
+      }
+    }
+
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.statusCode).toBe(302)
+    expect(postResponse.headers.location).toBe('planning-permission')
+  })
+
+  it('should display ineligible page when user response is \'No\'', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/country`,
+      payload: { projectPostcode: '', inEngland: 'No', crumb: crumbToken },
+      headers: {
+        cookie: 'crumb=' + crumbToken
+      }
+    }
+
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.payload).toContain('You cannot apply for a grant from this scheme')
+  })
+})
