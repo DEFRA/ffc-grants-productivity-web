41701315e1ed04ff337f50de962451fbdc88e7c1
Sal Abuewilly <sal.abuewilly@capgemini.com>
2023-10-10 09:12:28 +0100

    Bugfix/GRANTS-2756 and 2755 (#218)
    * Oh, well..
    
    * GRANTS-2755:
    Start page copy fixes
    
    * Update tests

--- test/integration/narrow/routes/start.test.js
+++ test/integration/narrow/routes/start.test.js
@@ -14,16 +14,22 @@ describe('Start page', () => {
     expect(response.payload).toContain('You can apply if you:')
     expect(response.payload).toContain('you are a farmer or agricultural contractor')
     expect(response.payload).toContain('your business is registered in England or you will do the grant-funded work in England')
-    expect(response.payload).toContain('estimated the project covers are over £37,500')
+    expect(response.payload).toContain(
+      "estimated the project covers are over £62,500"
+    );
     expect(response.payload).toContain('Agricultural contractors can only use the grant for buying robotic and automatic technology.')
     expect(response.payload).toContain('You can apply for up to:')
     expect(response.payload).toContain('40% of estimated costs of robotic and automatic technology')
     expect(response.payload).toContain('25% of estimated costs to buy and install a solar PV system')
-    expect(response.payload).toContain('The minimum grant you can claim is £15,000 (40% of £37,500).')
-    expect(response.payload).toContain('The maximum grant for:')
-    expect(response.payload).toContain('robotic and automatic technology is £500,000')
-    expect(response.payload).toContain('a solar PV system is £100,000')
-    expect(response.payload).toContain('If you want to apply for both robotic technology and solar PV system grant funding, you must submit 2 separate applications. The maximum grant amount for both projects together is £500,000.')
+    expect(response.payload).toContain(
+      "For robotic and automatic technology the maximum grant you can claim is £500,000. The minimum grant is £25,000."
+    );
+    expect(response.payload).toContain(
+      "For a solar PV system the maximum grant you can claim is £100,000. The minimum grant is £15,000."
+    );
+    expect(response.payload).toContain(
+      "If you want to apply for both robotic technology and solar PV system grant funding, you must submit 2 separate applications. The maximum grant amount for both projects together is £500,000."
+    );
     expect(response.payload).toContain('If your project is eligible, you will be able to submit your online application to the Rural Payments Agency (RPA).')
     expect(response.payload).toContain('Start now') // button
     expect(response.payload).toContain('Before you start')
