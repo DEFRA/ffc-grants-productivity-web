b5490db04bb236b0ae9d21bbe0512c71ca2dbe1b
Sal Abuewilly <sal.abuewilly@gmail.com>
2023-10-11 13:56:13 +0100

    update test

--- test/integration/narrow/routes/project-type.test.js
+++ test/integration/narrow/routes/project-type.test.js
@@ -80,6 +80,6 @@ describe('Project subject page', () => {
     }
     const response = await global.__SERVER__.inject(options)
     expect(response.statusCode).toBe(200)
-    expect(response.payload).toContain('Select what your project is about')
+    expect(response.payload).toContain('Select what you would like funding for')
   })
 })
