b70fb22f2abe5e761f8b4c0237e125a2e1e47b96
Yunus FIRAT <yunusfirat44@gmail.com>
2023-10-12 13:52:55 +0100

    comment in some more test.

--- test/integration/narrow/routes/existing-solar.test.js
+++ test/integration/narrow/routes/existing-solar.test.js
@@ -41,31 +41,31 @@ describe('Page: /existing-solar', () => {
     expect(postResponse.payload).toContain('No')
   })
 
-  // it('user selects \'Yes\' -> store user response and redirect to /solar-technologies', async () => {
-  //   const postOptions = {
-  //     method: 'POST',
-  //     url: `${global.__URLPREFIX__}/solar/existing-solar`,
-  //     headers: { cookie: 'crumb=' + crumbToken },
-  //     payload: { existingSolar: 'Yes', crumb: crumbToken }
-  //   }
+  it('user selects \'Yes\' -> store user response and redirect to /solar-technologies', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/solar/existing-solar`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: { existingSolar: 'Yes', crumb: crumbToken }
+    }
 
-  //   const postResponse = await global.__SERVER__.inject(postOptions)
-  //   expect(postResponse.statusCode).toBe(302)
-  //   expect(postResponse.headers.location).toBe('solar-technologies')
-  // })
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.statusCode).toBe(302)
+    expect(postResponse.headers.location).toBe('solar-technologies')
+  })
 
-  // it('user selects \'No\' -> store user response and redirect to solar-technologies', async () => {
-  //   const postOptions = {
-  //     method: 'POST',
-  //     url: `${global.__URLPREFIX__}/solar/existing-solar`,
-  //     headers: { cookie: 'crumb=' + crumbToken },
-  //     payload: { existingSolar: 'No', crumb: crumbToken }
-  //   }
+  it('user selects \'No\' -> store user response and redirect to solar-technologies', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/solar/existing-solar`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: { existingSolar: 'No', crumb: crumbToken }
+    }
 
-  //   const postResponse = await global.__SERVER__.inject(postOptions)
-  //   expect(postResponse.statusCode).toBe(302)
-  //   expect(postResponse.headers.location).toBe('solar-technologies')
-  // })
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.statusCode).toBe(302)
+    expect(postResponse.headers.location).toBe('solar-technologies')
+  })
 
   it('page loads with correct back link', async () => {
     varList.tenancy = 'Yes'
