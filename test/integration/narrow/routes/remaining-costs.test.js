5105518ccce455b07dcd5afaf227cecb677aba49
Yunus FIRAT <yunus.firat@capgemini.com>
2023-10-17 13:24:52 +0100

    feature/grants-2761 (#233)
    build 2.21.0
    https://github.com/DEFRA/ffc-grants-productivity-web/pull/233

---
+++ test/integration/narrow/routes/remaining-costs.test.js
@@ -0,0 +1,80 @@
+const { crumbToken } = require('./test-helper')
+
+describe('Page: /solar/remaining-costs', () => {
+  const varList = { 
+    projectCost: '150000', 
+    calculatedGrant: '37500', 
+    remainingCost: 112500
+  }
+
+  jest.mock('../../../../app/helpers/session', () => ({
+    setYarValue: (request, key, value) => null,
+    getYarValue: (request, key) => {
+      if (varList[key]) return varList[key]
+      else return undefined
+    }
+  }))
+
+  it('page loads successfully, with all the options', async () => {
+    const options = {
+      method: 'GET',
+      url: `${global.__URLPREFIX__}/solar/remaining-costs`
+    }
+
+    const response = await global.__SERVER__.inject(options)
+    expect(response.statusCode).toBe(200)
+    expect(response.payload).toContain('Can you pay the remaining costs of £112,500?')
+    expect(response.payload).toContain('Yes')
+    expect(response.payload).toContain('No')
+  })
+
+  it('no option selected -> show error message', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/solar/remaining-costs`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: { remainingCosts: '', crumb: crumbToken }
+    }
+
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.statusCode).toBe(200)
+    expect(postResponse.payload).toContain('Select yes if you can pay the remaining costs')
+  })
+
+  it('user selects ineligible option: \'No\' -> display ineligible page', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/solar/remaining-costs`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: { remainingCosts: 'No', crumb: crumbToken }
+    }
+
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.payload).toContain('You cannot apply for a grant from this scheme')
+  })
+
+  it('user selects eligible option: \'Yes\' -> store user response and redirect to /solar-usage', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/solar/remaining-costs`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: { remainingCosts: 'Yes', crumb: crumbToken }
+    }
+
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.statusCode).toBe(302)
+    expect(postResponse.headers.location).toBe('solar-usage')
+  })
+
+  it('page loads with correct back link - potential-amount', async () => {
+    varList.projectCost = 499999
+
+    const options = {
+      method: 'GET',
+      url: `${global.__URLPREFIX__}/solar/remaining-costs`
+    }
+    const response = await global.__SERVER__.inject(options)
+    expect(response.statusCode).toBe(200)
+    expect(response.payload).toContain('<a href=\"potential-amount\" class=\"govuk-back-link\">Back</a>')
+  })
+})
