9aacc7e48f7caae835dc822321bda0a2bc29e6b9
salfedev <sal.abuewilly@capgemini.com>
2023-10-24 21:41:20 +0100

    WIP fixing tests

--- app/helpers/urls.js
+++ app/helpers/urls.js
@@ -2,48 +2,55 @@ const urlPrefix = require('../config/server').urlPrefix
 const { getYarValue } = require('../helpers/session')
 const { ALL_QUESTIONS } = require('../config/question-bank')
 const { getQuestionAnswer } = require('../helpers/utils')
+
+const findDependentQuestion = (
+  dependentQuestionYarKey,
+  dependentAnswerKeysArray,
+  dependentAnswer
+) => {
+  return ALL_QUESTIONS.find((thisQuestion) => {
+    const hasMatchingAnswer = thisQuestion.answers?.some((answer) => {
+      return (
+        dependentAnswer &&
+        dependentAnswerKeysArray.includes(answer.key) &&
+        dependentAnswer.includes(answer.value)
+      );
+    });
+    return thisQuestion.yarKey === dependentQuestionYarKey && hasMatchingAnswer;
+  });
+};
 const getUrl = (urlObject, url, request, secBtn) => {
   const scorePath = `${urlPrefix}/score`
   const chekDetailsPath = `${urlPrefix}/check-details`
   const secBtnPath = secBtn === 'Back to score' ? scorePath : chekDetailsPath
   const isRobotics = getQuestionAnswer('project-subject', 'project-subject-A1')
+  const isInEngland = getQuestionAnswer('tenancy', 'tenancy-A1')
   if (!urlObject || secBtn) {
     return secBtn ? secBtnPath : url
   }
   const { dependentQuestionYarKey, secondDependentQuestionYarKey, dependentAnswerKeysArray, seconddependentAnswerKeysArray, urlOptions } = urlObject // just make it work for now
-  let { thenUrl, elseUrl } = urlOptions
-  const dependentQuestionYarKeys = [dependentQuestionYarKey].flat()
-  thenUrl = [thenUrl].flat()
-  let selectThenUrl
-  let thenUrlIndex = -1
+  let { thenUrl, elseUrl, nonDependentUrl } = urlOptions;
 
   if (getYarValue(request, 'projectSubject') === isRobotics) {
-    if (getYarValue(request, 'tenancy') === 'Yes' && elseUrl === 'project-responsibility') {
-      elseUrl = 'robotics/project-items'
+    if (
+      getYarValue(request, "tenancy") === isInEngland &&
+      elseUrl === "project-responsibility"
+    ) {
+      elseUrl = "robotics/project-items";
     }
   }
 
-  dependentQuestionYarKeys.every((dependantYarKey, index) => {
-    const selectedAnswer = getYarValue(request, dependantYarKey)
-    if (selectedAnswer !== null) {
-      selectThenUrl = ALL_QUESTIONS.find(question => (
-        question.yarKey === dependantYarKey &&
-        question.answers &&
-        question.answers.some(answer => (
-          !!selectedAnswer &&
-          dependentAnswerKeysArray.includes(answer.key) &&
-          (Array.isArray(selectedAnswer) ? selectedAnswer.includes(answer.value) : (selectedAnswer === answer.value))
-        ))
-      ))
-      if (selectThenUrl) {
-        thenUrlIndex = index
-        return false
-      }
-    }
-    return true
-  })
+  const dependentAnswer = getYarValue(request, dependentQuestionYarKey);
+  console.log("dependentAnswer: ", dependentAnswer);
 
-  return thenUrlIndex > -1 ? thenUrl[thenUrlIndex] : elseUrl
+  const selectThenUrl = findDependentQuestion(
+    dependentQuestionYarKey,
+    dependentAnswerKeysArray,
+    dependentAnswer
+  );
+  console.log("selectThenUrl: ", selectThenUrl);
+  const selectedElseUrl = dependentAnswer ? elseUrl : nonDependentUrl;
+  return selectThenUrl ? thenUrl : selectedElseUrl;
 }
 
 module.exports = {
--- test/integration/narrow/helpers/urls.test.js
+++ test/integration/narrow/helpers/urls.test.js
@@ -1,71 +1,88 @@
-const urlPrefix = require('../../../../app/config/server').urlPrefix
-describe('getUrl()', () => {
-  jest.mock('../../../../app/helpers/session')
-  const { getYarValue } = require('../../../../app/helpers/session')
+const urlPrefix = require("../../../../app/config/server").urlPrefix;
+describe("getUrl()", () => {
+  jest.mock("../../../../app/helpers/session");
+  const { getYarValue } = require("../../../../app/helpers/session");
 
-  const { getUrl } = require('../../../../app/helpers/urls')
-  let urlObject = null
-  let secBtn = 'Back to score'
-  let dict = {}
+  const { getUrl } = require("../../../../app/helpers/urls");
+  let urlObject = null;
+  let secBtn = "Back to score";
+  let dict = {};
   beforeEach(() => {
-    getYarValue.mockImplementation((req, key) => (dict[key]))
-  })
+    getYarValue.mockImplementation((req, key) => dict[key]);
+  });
   afterEach(() => {
-    jest.resetAllMocks()
-  })
-  it('should return url if urlObject is empty', () => {
-    expect(getUrl(urlObject, 'mock-url', {}, secBtn, '')).toEqual(`${urlPrefix}/score`)
+    jest.resetAllMocks();
+  });
+  it("should return url if urlObject is empty", () => {
+    expect(getUrl(urlObject, "mock-url", {}, secBtn, "")).toEqual(
+      `${urlPrefix}/score`
+    );
+    secBtn = "";
+    expect(getUrl(urlObject, "mock-url", {}, secBtn, "")).toEqual("mock-url");
+  });
 
-    secBtn = ''
-    expect(getUrl(urlObject, 'mock-url', {}, secBtn, '')).toEqual('mock-url')
-  })
-
-  it.skip('should return nonDependentUrl if urlObject is present, but yar values are empty', () => {
+  it.skip("should return nonDependentUrl if urlObject is present, but yar values are empty", () => {
     urlObject = {
-      dependentQuestionYarKey: 'dependentQuestionYarKey',
-      dependentAnswerKeysArray: 'dependentAnswerKeysArray',
+      dependentQuestionYarKey: "dependentQuestionYarKey",
+      dependentAnswerKeysArray: ["dependentAnswerKeysArray"],
       urlOptions: {
-        thenUrl: 'thenUrl',
-        elseUrl: 'elseUrl',
-        nonDependentUrl: 'nonDependentUrl'
-      }
-    }
-    expect(getUrl(urlObject, 'mock-url', {}, secBtn, '')).toEqual('nonDependentUrl')
-  })
-  it('should return elseUrl if urlObject and dependent Yar values are present', () => {
+        thenUrl: "thenUrl",
+        elseUrl: "elseUrl",
+        nonDependentUrl: "nonDependentUrl",
+      },
+    };
+    const selectedURL = getUrl(urlObject, "mock-url", {}, secBtn, "");
+    console.log("here: ", selectedURL);
+    expect(selectedURL).toEqual("nonDependentUrl");
+  });
+  it("should return elseUrl if urlObject and dependent Yar values are present", () => {
     urlObject = {
-      dependentQuestionYarKey: 'dependentQuestionYarKey',
-      dependentAnswerKeysArray: 'dependentAnswerKeysArray',
+      dependentQuestionYarKey: "dependentQuestionYarKey",
+      dependentAnswerKeysArray: "dependentAnswerKeysArray",
       urlOptions: {
-        thenUrl: 'thenUrl',
-        elseUrl: 'elseUrl',
-        nonDependentUrl: 'nonDependentUrl'
-      }
-    }
+        thenUrl: "thenUrl",
+        elseUrl: "elseUrl",
+        nonDependentUrl: "nonDependentUrl",
+      },
+    };
     dict = {
-      dependentQuestionYarKey: 'dependentAnswerKeysArray'
-    }
-    expect(getUrl(urlObject, 'mock-url', {}, secBtn, '')).toEqual('elseUrl')
-  })
+      dependentQuestionYarKey: "dependentAnswerKeysArray",
+    };
+    expect(getUrl(urlObject, "mock-url", {}, secBtn, "")).toEqual("elseUrl");
+  });
   it('should return secBtnPath if secBtn is "Back to score"', () => {
-    urlObject = null
+    urlObject = null;
     dict = {
-      dependentQuestionYarKey: 'dependentAnswerKeysArray'
-    }
-    expect(getUrl(urlObject, 'mock-url', {}, 'Back to score', '')).toEqual(`${urlPrefix}/score`)
-  })
-  // it('should navigate to /planning-permission-summary if secBtn is not "Back to score" and current url is /grid-reference', () => {
-  //   urlObject = null
-  //   dict = {
-  //     dependentQuestionYarKey: 'dependentAnswerKeysArray'
-  //   }
-  //   expect(getUrl(urlObject, 'mock-url', {}, 'i_hate_js', 'grid-reference')).toEqual(`${urlPrefix}/planning-permission-summary`)
-  // })
+      dependentQuestionYarKey: "dependentAnswerKeysArray",
+    };
+    expect(getUrl(urlObject, "mock-url", {}, "Back to score", "")).toEqual(
+      `${urlPrefix}/score`
+    );
+  });
+
   it('should default to /check-details if secBtn is not "Back to score" and current url is not a building or planning page', () => {
-    urlObject = null
+    urlObject = null;
     dict = {
-      dependentQuestionYarKey: 'dependentAnswerKeysArray'
-    }
-    expect(getUrl(urlObject, 'mock-url', {}, 'i-wish-i-was-writing-python', 'or-even-java')).toEqual(`${urlPrefix}/check-details`)
-  })
-})
+      dependentQuestionYarKey: "dependentAnswerKeysArray",
+    };
+    expect(
+      getUrl(
+        urlObject,
+        "mock-url",
+        {},
+        "i-wish-i-was-writing-python",
+        "or-even-java"
+      )
+    ).toEqual(`${urlPrefix}/check-details`);
+  });
+  it.skip("should redirect to /project-responsibility if user selected Robotics and is in England", () => {
+    urlObject = null;
+    dict = {
+      projectSubject: "Robotics and automatic technology",
+      tenancy: "Yes",
+    };
+    expect(getUrl(urlObject, "mock-url", {}, "", "")).toEqual(
+      "project-responsibility"
+    );
+  });
+});
