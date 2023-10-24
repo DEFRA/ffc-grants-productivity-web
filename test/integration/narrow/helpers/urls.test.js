5a1c8fd041997316309b8cfa777439795df6393d
Sal Abuewilly <sal.abuewilly@gmail.com>
2023-10-24 15:21:22 +0100

    * WIP better URL tests, * better test script

--- test/integration/narrow/helpers/urls.test.js
+++ test/integration/narrow/helpers/urls.test.js
@@ -1,20 +1,26 @@
-describe('urls.js', () => {
+const urlPrefix = require('../../../../app/config/server').urlPrefix
+describe('getUrl()', () => {
   jest.mock('../../../../app/helpers/session')
   const { getYarValue } = require('../../../../app/helpers/session')
 
   const { getUrl } = require('../../../../app/helpers/urls')
-
-  test('getUrl()', () => {
-    let dict = {}
+  let urlObject = null
+  let secBtn = 'Back to score'
+  let dict = {}
+  beforeEach(() => {
     getYarValue.mockImplementation((req, key) => (dict[key]))
-
-    let urlObject = null
-    let secBtn = 'Back to score'
-    expect(getUrl(urlObject, 'mock-url', {}, secBtn, '')).toEqual('/productivity/score')
+  })
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+  it('should return url if urlObject is empty', () => {
+    expect(getUrl(urlObject, 'mock-url', {}, secBtn, '')).toEqual(`${urlPrefix}/score`)
 
     secBtn = ''
     expect(getUrl(urlObject, 'mock-url', {}, secBtn, '')).toEqual('mock-url')
+  })
 
+  it.skip('should return nonDependentUrl if urlObject is present, but yar values are empty', () => {
     urlObject = {
       dependentQuestionYarKey: 'dependentQuestionYarKey',
       dependentAnswerKeysArray: 'dependentAnswerKeysArray',
@@ -24,9 +30,42 @@ describe('urls.js', () => {
         nonDependentUrl: 'nonDependentUrl'
       }
     }
+    expect(getUrl(urlObject, 'mock-url', {}, secBtn, '')).toEqual('nonDependentUrl')
+  })
+  it('should return elseUrl if urlObject and dependent Yar values are present', () => {
+    urlObject = {
+      dependentQuestionYarKey: 'dependentQuestionYarKey',
+      dependentAnswerKeysArray: 'dependentAnswerKeysArray',
+      urlOptions: {
+        thenUrl: 'thenUrl',
+        elseUrl: 'elseUrl',
+        nonDependentUrl: 'nonDependentUrl'
+      }
+    }
+    dict = {
+      dependentQuestionYarKey: 'dependentAnswerKeysArray'
+    }
     expect(getUrl(urlObject, 'mock-url', {}, secBtn, '')).toEqual('elseUrl')
-
-    dict = { dependentQuestionYarKey: 'dp-yarKey' }
-    expect(getUrl(urlObject, 'mock-url', {}, secBtn, '')).toEqual('elseUrl')
+  })
+  it('should return secBtnPath if secBtn is "Back to score"', () => {
+    urlObject = null
+    dict = {
+      dependentQuestionYarKey: 'dependentAnswerKeysArray'
+    }
+    expect(getUrl(urlObject, 'mock-url', {}, 'Back to score', '')).toEqual(`${urlPrefix}/score`)
+  })
+  // it('should navigate to /planning-permission-summary if secBtn is not "Back to score" and current url is /grid-reference', () => {
+  //   urlObject = null
+  //   dict = {
+  //     dependentQuestionYarKey: 'dependentAnswerKeysArray'
+  //   }
+  //   expect(getUrl(urlObject, 'mock-url', {}, 'i_hate_js', 'grid-reference')).toEqual(`${urlPrefix}/planning-permission-summary`)
+  // })
+  it('should default to /check-details if secBtn is not "Back to score" and current url is not a building or planning page', () => {
+    urlObject = null
+    dict = {
+      dependentQuestionYarKey: 'dependentAnswerKeysArray'
+    }
+    expect(getUrl(urlObject, 'mock-url', {}, 'i-wish-i-was-writing-python', 'or-even-java')).toEqual(`${urlPrefix}/check-details`)
   })
 })
