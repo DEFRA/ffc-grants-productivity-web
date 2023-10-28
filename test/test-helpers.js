758b7b7fb4c4b4736eaf8b230c621d645cd3f5f0
Sal Abuewilly <sal.abuewilly@gmail.com>
2023-10-28 05:25:25 +0100

    Linting

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -1,27 +1,26 @@
 const createPage = (textContent) => {
-  return new JSDOM(textContent).window.document;
-};
-
-const getQuestionH1 = (htmlPage) => {
-  return htmlPage.querySelector("h1.govuk-fieldset__heading");
-};
-
-const getQuestionCheckboxes = (htmlPage) => {
-  return htmlPage.querySelectorAll("input.govuk-checkboxes__input");
-};
-
-const getQuestionErrors = (htmlPage) => {
-  return htmlPage.querySelectorAll("ul.govuk-error-summary__list");
-};
-
-const getBackLink = (htmlPage) => {
-  return htmlPage.querySelector(".govuk-back-link");
-};
-
-const getQuestionRadios = (htmlPage) => {
-  return htmlPage.querySelectorAll("input.govuk-radios__input");
+  return new JSDOM(textContent).window.document
 }
 
+const getQuestionH1 = (htmlPage) => {
+  return htmlPage.querySelector('h1.govuk-fieldset__heading')
+}
+
+const getQuestionCheckboxes = (htmlPage) => {
+  return htmlPage.querySelectorAll('input.govuk-checkboxes__input')
+}
+
+const getQuestionErrors = (htmlPage) => {
+  return htmlPage.querySelectorAll('ul.govuk-error-summary__list')
+}
+
+const getBackLink = (htmlPage) => {
+  return htmlPage.querySelector('.govuk-back-link')
+}
+
+const getQuestionRadios = (htmlPage) => {
+  return htmlPage.querySelectorAll('input.govuk-radios__input')
+}
 
 module.exports = {
   createPage,
@@ -30,4 +29,4 @@ module.exports = {
   getQuestionErrors,
   getBackLink,
   getQuestionRadios
-};
+}
