ecdd52362ab7f683b455f370e62174566dba8d35
zavies97 <zavies97@users.noreply.github.com>
2023-11-03 15:40:40 +0000

    i think thats everything for an initial test?

--- app/messaging/scoring/create-desirability-msg.js
+++ app/messaging/scoring/create-desirability-msg.js
@@ -18,18 +18,45 @@ function getUserAnswer (answers, userInput) {
 function getDesirabilityDetails (questionKey, userInput) {
   const content = questionContent[questionKey]
   // needs to loop for eligibility-criteria for each array in answers array
-  console.log('content', questionContent[questionKey], questionKey)
-  return {
-    key: content[0].key,
-    answers: content.map(({ key, title, answers }) => ({
-      key,
-      title,
-      input: getUserAnswer(answers, userInput[questionKey])
-    })),
-    rating: {
-      score: null,
-      band: null,
-      importance: null
+  console.log('content', questionContent[questionKey], questionKey, userInput[questionKey])
+
+  if (questionKey === 'eligibilityCriteria') {
+    let loopLists = []
+    for (item in userInput[questionKey]) {
+
+      console.log('eligibility criteria item to be added', userInput[questionKey], userInput[questionKey][item])
+
+      loopLists.push({
+       answers: content.map(({ key, title, answers }) => ({
+          key,
+          title,
+          input: getUserAnswer(answers, userInput[questionKey][item])
+        })),
+        rating: {
+          score: null,
+          band: null,
+          importance: null
+        }
+      })
+    }
+    return {
+      key: content[0].key,
+      answers: loopLists
+    }
+
+  } else {
+    return {
+      key: content[0].key,
+      answers: content.map(({ key, title, answers }) => ({
+        key,
+        title,
+        input: getUserAnswer(answers, userInput[questionKey])
+      })),
+      rating: {
+        score: null,
+        band: null,
+        importance: null
+      }
     }
   }
 }
