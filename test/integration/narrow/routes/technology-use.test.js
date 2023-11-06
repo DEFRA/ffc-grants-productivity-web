3ddcf6e7ab8e804dfccff7aff5a30e7bb7745451
zavies97 <zavies97@users.noreply.github.com>
2023-11-06 16:37:58 +0000

    test fixes

--- test/integration/narrow/routes/technology-use.test.js
+++ test/integration/narrow/routes/technology-use.test.js
@@ -29,7 +29,21 @@ describe('Robotics technology-use page', () => {
     expect(postResponse.payload).toContain('Select if you are already using this technology')
   })
 
-  it('store user response and redirect to energy source page', async () => {
+  it('store user response and redirect to scorepage', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/technology-use`,
+      payload: { technologyUse: 'some fake technology', crumb: crumbToken },
+      headers: { cookie: 'crumb=' + crumbToken }
+    }
+
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.statusCode).toBe(302)
+    expect(postResponse.headers.location).toBe('score')
+  })
+
+  it('store user response and redirect to labour-replaced page', async () => {
+    varList.projectItems = ['Robotic and automatic technology'] 
     const postOptions = {
       method: 'POST',
       url: `${global.__URLPREFIX__}/technology-use`,
