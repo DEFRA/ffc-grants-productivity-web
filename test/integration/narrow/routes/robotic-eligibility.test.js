ab13c1f34f260d7d33da3ad2eeb13cf1a73a3e10
Sal Abuewilly <sal.abuewilly@gmail.com>
2023-11-16 06:53:36 +0000

    add tests

--- test/integration/narrow/routes/robotic-eligibility.test.js
+++ test/integration/narrow/routes/robotic-eligibility.test.js
@@ -71,15 +71,15 @@ expect(postResponse.payload).toContain('RPA will only fund robotic technology th
 })
 
 it('user selects \'Other robotics or automatic technology\' -> title should be \'Does your robotic technology fit the eligibility criteria?\'', async () => {
-varList.technologyItems = 'Other robotics or automatic technology'
-const options = {
-    method: 'GET',
-    url: `${global.__URLPREFIX__}/robotic-eligibility`
-}
+  varList.technologyItems = 'Other robotics or automatic technology'
+  const options = {
+      method: 'GET',
+      url: `${global.__URLPREFIX__}/robotic-eligibility`
+  }
 
-const response = await global.__SERVER__.inject(options)
-expect(response.statusCode).toBe(200)
-expect(response.payload).toContain('Does your robotic technology fit the eligibility criteria?')
+  const response = await global.__SERVER__.inject(options)
+  expect(response.statusCode).toBe(200)
+  expect(response.payload).toContain('Does your robotic technology fit the eligibility criteria?')
 })
 
 it('should display ineligible page with "Add another item" and "Continue with eligible items" buttons, if user selects No for the second project item', async () => {
@@ -108,4 +108,22 @@ const response = await global.__SERVER__.inject(options)
 expect(response.statusCode).toBe(200)
 expect(response.payload).toContain('<a href=\"robotic-automatic\" class=\"govuk-back-link\">Back</a>')
 })
+
+const title_dict = {
+  'Slurry and manure management': 'Do your slurry robots fit the eligibility criteria?',
+  'Driverless tractor': 'Does your driverless robotic tractor or platform fit the eligibility criteria?',
+  'Voluntary robotic milking system': 'Does your voluntary robotic milking system fit the eligibility criteria?',
+}
+  Object.keys(title_dict).forEach(async (key) => {
+   it(`If user selected "${key}", title should change to "${title_dict[key]}"`, async () => {
+      varList.technologyItems = key
+      const options = {
+        method: 'GET',
+        url: `${global.__URLPREFIX__}/robotic-eligibility`
+      }
+      const response = await global.__SERVER__.inject(options)
+      expect(response.statusCode).toBe(200)
+      expect(response.payload).toContain(title_dict[key])
+    })
+  })
 })
